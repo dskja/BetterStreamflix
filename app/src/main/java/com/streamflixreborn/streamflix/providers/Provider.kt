b619e5c6f07a3ad06e4ecc9e178bf82a6969fa82
PaduU29 <PaduU29@users.noreply.github.com>
2026-07-24 08:14:33 +0200

    feat: add FanpelisProvider

--- app/src/main/java/com/streamflixreborn/streamflix/providers/Provider.kt
+++ app/src/main/java/com/streamflixreborn/streamflix/providers/Provider.kt
@@ -61,6 +61,7 @@ interface Provider {
 
         val providers = mapOf(
             SflixProvider to ProviderSupport(movies = true, tvShows = true),
+            FanpelisProvider to ProviderSupport(movies = true, tvShows = true),
             SerienStreamProvider to ProviderSupport(movies = false, tvShows = true),
             StreamingCommunityProvider("it") to ProviderSupport(movies = true, tvShows = true),
             StreamingCommunityProvider("en") to ProviderSupport(movies = true, tvShows = true),
